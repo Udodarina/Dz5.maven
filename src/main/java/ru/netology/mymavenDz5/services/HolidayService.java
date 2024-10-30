b6d40df29840e2eda97aa8d88918ffff810ca434
Arina Udod <audod6150@gmail.com>
2024-10-30 17:24:19 +0300

    First commit

---
+++ src/main/java/ru/netology/mymavenDz5/services/HolidayService.java
@@ -0,0 +1,19 @@
+package ru.netology.mymavenDz5.services;
+public class HolidayService {
+
+        public int calculate(int income, int expenses, int threshold) {
+            int count = 0; // счетчик месяцев отдыха
+            int money = 0; // количество денег на счету
+            for (int month = 0; month < 12; month++) {
+                if (money >= threshold) // условие для месяца отдыха
+                     {
+                    count++; // увеличивает счетчик месяцев отдыха
+                    money = (money - expenses) / 3; // количество денег при отдыхе
+                } else {
+                    money = money + income - expenses; // количество денег при работе
+                }
+            }
+            return count;
+        }
+    }
+
---
+++ src/test/java/ru/netology/mymavenDz5/services/HolidayServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.mymavenDz5.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HolidayServiceTest {
+
+    @Test
+    public void HolidayWithLowSalary () {
+        int expected=3;
+        int income=10_000;
+        int expenses=3_000;
+        int threshold=20_000;
+        HolidayService service = new HolidayService();
+        int actual = (int) service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void HolidayWithHighSalaru () {
+        int expected=2;
+        int income=100_000;
+        int expenses=60_000;
+        int threshold=150_000;
+        HolidayService service = new HolidayService();
+        int actual = (int) service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
+}
